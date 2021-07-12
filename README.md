340fedfe25fa080aaeb7112cb6dee91a788b42df
Ali Shah <s.ashah@outlook.com>
2021-07-11 20:08:20 -0400

    [Added] /weather route
    NOTES:
    - Supports API call to `/weather` with `city` query parameter
    - Examples for `city` query parameter:
      - `GET http://localhost:3000/weather?city=Toronto`
      - `GET http://localhost:3000/weather?city=Hamilton,CA`
      - `GET http://localhost:3000/weather?city=Houston,US`

--- README.md
+++ README.md
@@ -4,6 +4,22 @@
 
 Use any backend framework to build a weather app using the OpenWeatherMap API. Allow anonymous users to input their mood each day corresponding to the weather.
 
-## Usage
+## Prerequisites
 
 To run the app locally, an OpenWeatherMap API key is necessary
+
+## Endpoints
+
+BASE URL: `http://localhost:YOUR_PORT`
+
+### Weather by city
+
+Enter `city` query paramater (`city_name`, or `city_name,country_code`)
+
+`GET /weather?city=Toronto`
+`GET /weather?city=Hamilton,CA`
+`GET /weather?city=Houston,US`
+
+### Enter your mood
+
+Coming soon
--- src/app.js
+++ src/app.js
@@ -1,14 +1,27 @@
 require('dotenv').config();
 const express = require('express');
+const fetch = require('node-fetch');
 
 const app = express();
 
 // ROUTES
-app.get('/', (req, res) => {
-  res.send('Salaam, World :)');
+app.get('/weather', async (req, res) => {
+  const URL = 'http://api.openweathermap.org/data/2.5/weather';
+  const { city } = req.query;
+
+  fetch(`${URL}?q=${city}&appid=${process.env.API_KEY}`)
+    .then((results) => results.json())
+    .then((json) => {
+      res.json(json);
+    })
+    .catch((err) => {
+      res.json({ err });
+    });
+});
+
+app.get('/:asdf/:mood', (req, res) => {
+  res.send('Not yet implemented');
 });
 
 // START SERVER
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Listening on port ${process.env.PORT || 3000}`);
-});
+app.listen(process.env.PORT || 3000);
